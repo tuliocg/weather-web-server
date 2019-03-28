1faaab0e775eaa012758926338d8549ea9fd35a8
Túlio Gonçalves <tulio@fernbi.com.br>
2019-03-28 14:29:45 -0300

    fix console.log  app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side java script is loaded');
-
 fetch('http://localhost:3000/weather?address=florianopolis').then((response) => {
     response.json().then((data) =>  {
         if(data.error)
