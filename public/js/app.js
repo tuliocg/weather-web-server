ac1f57c9f37a1c736722ea60edb156a9c37a834b
Túlio Gonçalves <tulio@fernbi.com.br>
2019-03-28 20:54:36 -0300

    fix arquivox async dropbox

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,33 @@
-fetch('http://localhost:3000/weather?address=florianopolis').then((response) => {
+console.log('client side java script is loaded');
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    message1.textContent = 'Loading Forecast for '+ location
+    message2.textContent = ''
+
+    
+
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
     response.json().then((data) =>  {
         if(data.error)
         {
-            console.log(data.error)
+            message1.textContent = data.error
+            
         } else {
-            console.log(data.forecast)
-            console.log(data.location);
+            message2.textContent = data.location
+            message1.textContent = data.forecast
+            
         }  
     })
 })
+    
+}
+)
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //paths settings
 const publicDirectory = path.join(__dirname, '../public')
@@ -97,7 +98,7 @@ app.get('*', (req, res) => {
     )
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000');
+app.listen(port, () => {
+    console.log('server is up on port '+port);
     
 })
